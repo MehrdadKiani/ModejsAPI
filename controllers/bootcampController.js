50a913a9152bae6162a67339c7e74c4888d309c7
Mehrdad <mehrdad.kiani.eng@gmail.com>
2020-08-01 21:02:59 +0430

    new api added (resetData)

--- controllers/bootcampController.js
+++ controllers/bootcampController.js
@@ -45,6 +45,17 @@ exports.createNewBootcamp = async (req, res, next) => {
     }
 }
 
+exports.resetData = async (req, res, next) => {
+    const data = [{ "name": "Devworks Bootcamp", "description": "Devworks is a full stack JavaScript Bootcamp located in the heart of Boston that focuses on the technologies you need to get a high paying job as a web developer", "website": "https://devworks.com", "phone": "(111) 111-1111", "email": "enroll@devworks.com", "address": "233 Bay State Rd Boston MA 02215", "careers": ["Web Development", "UI/UX", "Business"], "housing": true, "jobAssistance": true, "jobGuarantee": false, "acceptGi": true }, { "name": "ModernTech Bootcamp", "description": "ModernTech has one goal, and that is to make you a rockstar developer and/or designer with a six figure salary. We teach both development and UI/UX", "website": "https://moderntech.com", "phone": "(222) 222-2222", "email": "enroll@moderntech.com", "address": "220 Pawtucket St, Lowell, MA 01854", "careers": ["Web Development", "UI/UX", "Mobile Development"], "housing": false, "jobAssistance": true, "jobGuarantee": false, "acceptGi": true }, { "name": "Codemasters", "description": "Is coding your passion? Codemasters will give you the skills and the tools to become the best developer possible. We specialize in full stack web development and data science", "website": "https://codemasters.com", "phone": "(333) 333-3333", "email": "enroll@codemasters.com", "address": "85 South Prospect Street Burlington VT 05405", "careers": ["Web Development", "Data Science", "Business"], "housing": false, "jobAssistance": false, "jobGuarantee": false, "acceptGi": false }, { "name": "Devcentral Bootcamp", "description": "Is coding your passion? Codemasters will give you the skills and the tools to become the best developer possible. We specialize in front end and full stack web development", "website": "https://devcentral.com", "phone": "(444) 444-4444", "email": "enroll@devcentral.com", "address": "45 Upper College Rd Kingston RI 02881", "careers": ["Mobile Development", "Web Development", "Data Science", "Business"], "housing": false, "jobAssistance": true, "jobGuarantee": true, "acceptGi": true }];
+    try {
+        await BootcampModel.deleteMany();
+        const result = await BootcampModel.insertMany(data);
+        res.status(201).json({ success: true, data: result });
+    } catch (error) {
+        next(error);
+    }
+}
+
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
@@ -88,6 +99,11 @@ exports.deleteBootcampById = async (req, res, next) => {
 // @desc    Delete all bootcamps
 // @route   DELETE /api/v1/bootcamps
 // @access  Private
-exports.deleteAllBootcamps = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Delete all bootcamps` });
+exports.deleteAllBootcamps = async (req, res, next) => {
+    try {
+        const result = await BootcampModel.deleteMany();
+        res.status(200).json({ success: true, data: result });
+    } catch (error) {
+        next(error);
+    }
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 "use stict";
 
 const express = require('express');
-const { getAllBootcamps, getBootcampById, createNewBootcamp, updateAllBootcamps, updateBootcampById, deleteAllBootcamps, deleteBootcampById } = require('../controllers/bootcampController')
+const { getAllBootcamps, getBootcampById, createNewBootcamp, updateAllBootcamps, updateBootcampById, deleteAllBootcamps, deleteBootcampById, resetData } = require('../controllers/bootcampController')
 
 router = express.Router();//Express Router middleware
 
@@ -15,5 +15,8 @@ router.route('/api/v1/bootcamps/:id')
     .get(getBootcampById)
     .put(updateBootcampById)
     .delete(deleteBootcampById);
+
+router.route('/api/v1/bootcamps/reset')
+    .post(resetData);
 
 module.exports = router;
